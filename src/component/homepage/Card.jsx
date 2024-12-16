b32d8580bd6683e743252dcd95533bd3c96b6d16
Ehan <admin@admins-iMac.local>
2024-12-16 14:20:35 +0000

    task-4 article render

--- src/component/homepage/Card.jsx
+++ src/component/homepage/Card.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
-function Card() {
+function Card({ article }) {
   return (
-    <div>This Card Section</div>
-  )
+    <div className="container-fluid py-3" style={{ animation: "fadeIn 3s" }}>
+      <h1 className="py-1 fs-1">Articles</h1>
+      <div className="row row-cols-1 row-cols-sm-6 g-5">
+            {article.map(item => {
+                return( <div className="col-sm-2">
+          <div className="card h-100">
+            <img src={item.article_img_url} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5>
+                Author Name: <span className="fs-6">{item.author}</span>
+              </h5>
+              <h5>
+                article_id: <span className="fs-6">{item.article_id}</span>
+              </h5>
+              <h5>
+                Title:
+                <span className="fs-6">{item.title}</span>
+              </h5>
+              <h5>Topic: <span className="fs-6">{item.topic}</span></h5>
+              <h5>
+                created at:
+                <span className="fs-6">{item.created_at}</span>
+              </h5>
+              <h5>
+                Votes: <span className="fs-6">{item.votes}</span>
+              </h5>
+              <h5>
+                Comment Count:
+                <span className="fs-6">{item.comment_count}</span>
+              </h5>
+            </div>
+          </div>
+        </div>)
+
+            })}
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
--- src/component/homepage/Home.jsx
+++ src/component/homepage/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './Card'
+import axios from 'axios'
 function Home() {
+  const [article, Setarticle] = useState([])
+  const url = 'https://mynew-nc-news.onrender.com/api/articles'
+useEffect(()=>{
+axios.get(url)
+.then((res)=>{
+Setarticle(res.data.article);
+})
+},[])
+
   return (
     <>
- <Card/>
+ <Card article = {article}/>
   </>
   )
 }
